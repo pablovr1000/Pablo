f6bcc87a80305ac6b13c658338fcddb70898fd64
pablovr1000 <pablovr1000@hotmail.com>
2021-12-31 08:55:42 -0300

    Trabalhando com Double no CSharp

---
+++ AprendendoCSharp/AprendendoCSharp/3-CriandoVariáveisDouble/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double salario;
+        salario = 1817.99;
+
+        Console.WriteLine("o salário é R$" + salario + ", parabéns!");
+    }
+}
