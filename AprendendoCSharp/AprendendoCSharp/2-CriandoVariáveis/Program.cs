5ccde6d014e7e78cf672c03b45d3c998a7afb525
pablovr1000 <pablovr1000@hotmail.com>
2021-12-31 08:26:18 -0300

    adicionando projetos aprendendo CSharp

---
+++ AprendendoCSharp/AprendendoCSharp/2-CriandoVariáveis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("ola novo teste");
+
+        int idade = 37;
+
+        Console.WriteLine("a idade é " + idade + ", parabéns!");
+    }
+}
