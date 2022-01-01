c4c1ddc78c93b6e81c35512fcecc95b59c8311e8
pablovr1000 <pablovr1000@hotmail.com>
2022-01-01 11:35:05 -0300

    Aprendendo CSharp Parte 02

---
+++ AprendendoCSharp/ByteBank/03-ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaCorrente contaDoPablo = new ContaCorrente();
+
+        contaDoPablo.titular = "Pablo";
+        contaDoPablo.numeroAgencia = 863;
+        contaDoPablo.numeroConta = 863146;
+
+        ContaCorrente contaDoPabloRios = new ContaCorrente();
+
+        contaDoPabloRios.titular = "Pablo";
+        contaDoPabloRios.numeroAgencia = 863;
+        contaDoPabloRios.numeroConta = 863146;
+
+        Console.WriteLine(contaDoPablo.titular);
+        Console.WriteLine("Agência: " + contaDoPablo.numeroAgencia);
+        Console.WriteLine("Conta: " + contaDoPablo.numeroConta);
+        Console.WriteLine("Saldo: " + contaDoPablo.saldo);
+
+        Console.WriteLine(contaDoPablo == contaDoPabloRios);
+
+        Console.ReadLine();
+    }
+}
